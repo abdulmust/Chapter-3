55716e9b267bd79d6c9b9717029c2241a0243c64
abdulmust <abdullahmustapha155@gmail.com>
2023-01-02 18:08:21 +0100

    modified raw codes to functional codes

--- shrinking_guest_list.py
+++ shrinking_guest_list.py
@@ -5,7 +5,15 @@ def family():
     third = guests.pop(3)
     fourth = guests.pop(2)
     fifth = guests.pop(-1)
-
+    removed = []
+    removed.append(first)
+    removed.append(second)
+    removed.append(third)
+    removed.append(fourth)
+    removed.append(fifth)
+    for i in removed:
+        text = f"Sorry, but I couldn't invite you {i.title()}"
+        print(text)
     for guest in guests:
         message = f"You have been selected to come with me for the dinner {guest.title()}."
         print(message)
